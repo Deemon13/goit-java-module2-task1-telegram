11ea2f5d1c20c8d4eea2c2f9004618b000140001
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-04 22:20:12 +0200

    added class MarsCalculator

---
+++ src/main/java/MarsCalculator.java
@@ -0,0 +1,34 @@
+class MarsCalculator {
+
+    public int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int sub(int a, int b, int c) {
+        return a - b - c;
+    }
+
+    public int multiply(int a, int b, int c) {
+        return sum(a, b, c)*sub(a,b,c);
+    }
+
+    public float divide(int a, int b, int c) {
+        return sum(a, b, c)/10f;
+    }
+
+    public static void main(String[] args) {
+        MarsCalculator calc = new MarsCalculator();
+
+        //Should be 6
+        System.out.println("math.sum(1, 2, 3) = " + calc.sum(1, 2, 3));
+
+        //Should be -4
+        System.out.println("math.sub(1, 2, 3) = " + calc.sub(1, 2, 3));
+
+        //Should be -24
+        System.out.println("math.multiply(1, 2, 3) = " + calc.multiply(1, 2, 3));
+
+        //Should be 0.6
+        System.out.println("math.divide(1, 2, 3) = " + calc.divide(1, 2, 3));
+    }
+}
