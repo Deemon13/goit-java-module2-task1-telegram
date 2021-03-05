90e51dcd1319ad5c05d53e73824e60756519aab1
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-05 20:09:27 +0200

    added method countAppleCount in class MarsEarthHelper

--- src/main/java/MarsEarthHelper.java
+++ src/main/java/MarsEarthHelper.java
@@ -1,7 +1,10 @@
 public class MarsEarthHelper {
     public int convertAppleCount(float count) {
-        int number = (int) count;
-        return number;
+        return (int) count;
+    }
+
+    public byte countAppleCount(int garden1Count, int garden2Count) {
+        return (byte) (garden1Count + garden2Count);
     }
 
     public static void main(String[] args) {
@@ -13,5 +16,12 @@ public class MarsEarthHelper {
         System.out.println("convertAppleCount(3.4f) = " + helper.convertAppleCount(3.4f));
         //Should be 10
         System.out.println("convertAppleCount(10.99) = " + helper.convertAppleCount(10.99f));
+
+        //Should be 17
+        System.out.println("countAppleCount(10, 7) = " + helper.countAppleCount(10, 7));
+        //Should be 40
+        System.out.println("countAppleCount(10, 30) = " + helper.countAppleCount(10, 30));
+        //Should be 51
+        System.out.println("countAppleCount(50, 1) = " + helper.countAppleCount(50, 1));
     }
 }
