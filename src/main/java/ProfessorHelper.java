972f8febbfde64f4f76689f8aa0d912a75c6255d
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-05 17:04:53 +0200

    added method canPrincessMarry in class ProfessorHelper

--- src/main/java/ProfessorHelper.java
+++ src/main/java/ProfessorHelper.java
@@ -8,12 +8,22 @@ class ProfessorHelper {
         return first * 5 + 3 == second;
     }
 
+    public boolean canPrincessMarry(int day) {
+        return day > 15;
+    }
+
     //Test output
     public static void main(String[] args) {
         ProfessorHelper helper = new ProfessorHelper();
+
         System.out.println("areNumbersCool(3, 3) = " + helper.areNumbersCool(3, 3)); //Should be true
         System.out.println("areNumbersCool(5, 7) = " + helper.areNumbersCool(5, 7)); //Should be false
+
         System.out.println("areNumbersFatal(1, 8) = " + helper.areNumbersFatal(1, 8)); //Should be true
         System.out.println("areNumbersFatal(1, 9) = " + helper.areNumbersFatal(1, 9)); //Should be false
+
+        System.out.println("canPrincessMarry(20) = " + helper.canPrincessMarry(20)); //Should be true
+        System.out.println("canPrincessMarry(16) = " + helper.canPrincessMarry(16)); //Should be true
+        System.out.println("canPrincessMarry(4) = " + helper.canPrincessMarry(4)); //Should be false
     }
 }
