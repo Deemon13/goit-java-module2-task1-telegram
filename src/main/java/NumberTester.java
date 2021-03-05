ab12098099726ca48aba07f968c83ea6bb4c8d10
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-05 17:33:56 +0200

    added class NumberTester

---
+++ src/main/java/NumberTester.java
@@ -0,0 +1,13 @@
+class NumberTester {
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        NumberTester tester = new NumberTester();
+        System.out.println(tester.isEven(10)); //Should be true
+        System.out.println(tester.isEven(4)); //Should be true
+        System.out.println(tester.isEven(5)); //Should be false
+    }
+}
