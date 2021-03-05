75172ab01bab8b2fc604ef9891e9da40a237786c
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-05 18:59:57 +0200

    added method canJumpOverHill in class MoonRobot

--- src/main/java/MoonRobot.java
+++ src/main/java/MoonRobot.java
@@ -3,6 +3,10 @@ public class MoonRobot {
         return holeDepth < 34 || batteryPercent > 67;
     }
 
+    public boolean canJumpOverHill(int hillHeight, int batteryPercent) {
+        return hillHeight <= 150 && batteryPercent >= 90;
+    }
+
     //Test output
     public static void main(String[] args) {
         MoonRobot robot = new MoonRobot();
@@ -11,5 +15,10 @@ public class MoonRobot {
         System.out.println("canOvercomeHole(50, 60) = " + robot.canOvercomeHole(50, 60));
         //Should be true
         System.out.println("canOvercomeHole(100, 80) = " + robot.canOvercomeHole(100, 80));
+
+        //Should be true
+        System.out.println("canJumpOverHill(100, 90) = " + robot.canJumpOverHill(100, 90));
+        //Should be false
+        System.out.println("canJumpOverHill(180, 100) = " + robot.canJumpOverHill(180, 100));
     }
 }
